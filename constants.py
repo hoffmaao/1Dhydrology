23c7f0a092366f0c9a59924f0a9dbd9588267919
Andrew Hoffman <hoffmaao@uw.edu>
2019-06-01 00:55:22 -0700

    model commit

---
+++ constants.py
@@ -0,0 +1,40 @@
+# Copyright (C) 2017-2018 Andrew Hoffman hoffmaao@uw.edu
+# Constants for hydrology model
+
+### Create a dictionary of physical constants
+pcs = {}
+
+# Seconds per day
+pcs['spd'] = 60.0 * 60.0 * 24.0
+# Seconds in a year
+pcs['spy'] = 60.0 * 60.0 * 24.0 * 365.0                    
+# Density of water (kg / m^3)
+pcs['rho_water'] = 1000.0  
+# Density of ice (kg / m^3)
+pcs['rho_ice'] = 910.0
+# Gravitational acceleration (m / s^2)
+pcs['g'] = 9.81 
+# Flow rate factor of ice (1 / Pa^3 * s) 
+pcs['A'] = 5.0e-25
+# Average bump height (m)
+pcs['h_r'] = 0.1
+# Typical spacing between bumps (m)
+pcs['l_r'] = 8.0
+# Sheet width under channel (m)
+pcs['l_c'] = 2.0          
+# Sheet conductivity (m^(7/4) / kg^(1/2))
+pcs['k'] = 1e-2
+# Channel conductivity (m^(7/4) / kg^(1/2))
+pcs['k_c'] = 1e-1
+# Specific heat capacity of ice (J / (kg * K))
+pcs['c_w'] = 4.22e3
+# Pressure melting coefficient (J / (kg * K))
+pcs['c_t'] = 7.5e-8
+# Latent heat (J / kg)
+pcs['L'] = 3.34e5
+# Void storage ratio
+pcs['e_v'] = 1e-3
+# Exponents 
+pcs['alpha'] = 5. / 4.
+pcs['beta'] = 3. / 2.
+pcs['delta'] = pcs['beta'] - 2.0
---
+++ hs_solver.py
@@ -0,0 +1,147 @@
+import firedrake
+from scipy.integrate import ode
+import numpy as np
+
+"""Solves ODEs for the sheet height h and channel area S."""
+
+class HSSolver():
+
+  def __init__(self, model):
+    
+    ### Get a few fields and parameters from the model
+    
+    # Effective pressure
+    N = firedrake.interpolate(model.N,model.V_cg)
+    # hydropotential
+    phi = firedrake.interpolate(model.phi,model.V_cg) 
+    # Cavity gap height
+    h = firedrake.interpolate(model.h,model.V_cg)
+    # Channel height
+    S = firedrake.interpolate(model.S,model.V_cg)
+    # Initial model time
+    t0 = model.t
+    # Rate factor
+    A = model.pcs['A']
+    # Distance between bumps
+    l_r = model.pcs['l_r']
+    # Bump height
+    h_r = model.pcs['h_r']
+    # Density of ice
+    rho_i = model.pcs['rho_ice']
+    # Latent heat
+    L = model.pcs['L']
+    # Sheet conductivity
+    k = model.pcs['k']
+    # Channel conductivity
+    k_c = model.pcs['k_c']
+    # Sheet width under channel
+    l_c = model.pcs['l_c']
+    # Exponent
+    alpha = model.pcs['alpha']
+    delta = model.pcs['delta']
+    # Regularization parameter
+    phi_reg = firedrake.Constant(1e-16)   
+    
+    
+    ### Static arrays used in the ODE rhs
+    
+    # Vector for sliding speed
+    u_b_n = firedrake.interpolate(model.u_b,model.V_cg)
+    u_b_n = u_b_n.vector().array()
+    h0 = firedrake.interpolate(model.h,model.V_cg)#model.h.vector().array()#firedrake.interpolate(model.h,model.V_cg)
+    h0 = h0.vector().array()
+    # Initial channel areas
+    S0 = firedrake.interpolate(model.S,model.V_cg)
+    S0 = S0.vector().array()
+    # Length of h vector
+    h_len = len(h0) 
+    print(h_len)
+
+    ### Set up the sheet height and channel area ODEs
+    
+    # Right hand side for the gap height ODE
+    def h_rhs(t, h_n) :
+      # Ensure that the sheet height is positive
+      h_n[h_n< 0.0] = firedrake.Constant(0.0)
+      # Sheet opening term
+      w_n = u_b_n * (h_r - h_n) / l_r
+      # Ensure that the opening term is non-negative
+      w_n[w_n < 0.0] = firedrake.Constant(0.0)
+      # Sheet closure term
+      v_n = A * h_n * N.vector().array()**firedrake.Constant(3.0)
+      # Return the time rate of change of the sheet
+      dhdt = w_n - v_n
+      return dhdt
+      
+    # Right hand side for the channel area ODE
+    def S_rhs(t, S_n):
+      # Ensure that the channel area is positive
+      S_n[S_n < 0.0] = firedrake.Constant(0.0)
+      # Get effective pressures
+      N_n = N.vector().array()
+      # Get midpoint values of sheet thickness
+      h_n = h.vector().array()
+      # Array form of the derivative of the potential 
+      phi_grad = model.phi.dx(0)
+      phi_s = firedrake.interpolate(phi_grad,model.V_cg)
+      #phi_s = phi.dx(0) # This is one obvious problem (How do we define derivatives of arrays?)
+      
+      # Along channel flux
+      Q_n = -firedrake.Constant(k_c) * S_n**firedrake.Constant(alpha) * np.abs(phi_s.dat.data + phi_reg)**firedrake.Constant(delta) * phi_s.dat.data
+      # Flux of sheet under channel
+      q_n = -firedrake.Constant(k) * h_n**firedrake.Constant(alpha) * np.abs(phi_s.dat.data + phi_reg)**firedrake.Constant(delta) * phi_s.dat.data
+      # Dissipation melting due to turbulent flux
+      # Creep closure
+      Xi_n = abs(Q_n * phi_s.dat.data) + np.abs(l_c * q_n * phi_s.dat.data)
+      v_c_n = A * S_n * N_n**3
+      # Total opening rate
+      v_o_n = Xi_n / (rho_i * L) # firedrake.conditional(firedrake.And(firedrake.lt(Xi_n / (rho_i * L),0.0),firedrake.eq(S_n,0.0)),0.0,Xi_n / (rho_i * L))
+      # Disallow negative opening rate where the channel area is 0
+      # v_o_n[v_o_n[firedrake.eq(S_n,0.0)] < 0.0] = 0.0
+      #v_o_n = firedrake.conditional(firedrake.And(firedrake.eq(S_n,0.0),le(Xi_n / (rho_i * L),0.0)),0.0,Xi_n / (rho_i * L))
+      # Calculate rate of channel size change
+      dsdt = (v_o_n - v_c_n)
+      return dsdt
+      
+    # Combined right hand side for h and S
+    def rhs(t, Y):
+      Ys = np.split(Y, [h_len])
+      h_n = Ys[0]
+      S_n = Ys[1]
+      
+      dsdt = S_rhs(t, S_n)
+      dhdt = h_rhs(t, h_n)
+      
+      return np.hstack((dhdt, dsdt))
+    
+    # ODE solver initial condition
+    Y0 = np.hstack((h0, S0))
+    # Set up ODE solver
+    ode_solver = ode(rhs).set_integrator('vode', method = 'adams', max_step = 60.0 * 5.0)
+    ode_solver.set_initial_value(Y0, t0)
+
+
+    ### Set local variables
+    self.Y0=Y0    
+    self.rhs=rhs
+    self.S_rhs= S_rhs
+    self.ode_solver = ode_solver
+    self.model = model
+    self.h_len = h_len
+    
+  def step(self, dt):
+    # Step h and S forward   
+    self.ode_solver.integrate(self.model.t + dt)
+
+    # Retrieve values from the ODE solver    
+    Y = np.split(self.ode_solver.y, [self.h_len])
+    self.model.h.vector().set_local(Y[0])
+    self.model.h.vector().apply("insert")
+    self.model.S.vector().set_local(Y[1])
+    self.model.S.vector().apply("insert")
+    
+    # Update S**alpha
+    self.model.update_S_alpha()
+  
+    # Update the model time
+    self.model.t = self.ode_solver.t
